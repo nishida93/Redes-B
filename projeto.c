cb0703249d2ded243a62c2c0ea509619fe3a601f
Louiz <luizhs396@gmail.com>
2015-08-23 14:28:06 -0300

    loading file

---
+++ projeto.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+   struct cadastro
+{
+    char user[10];
+    char mens[50];    
+    int op;
+};
+
+/*
+ * Cliente UDP
+ */
+int main(int argc, char *argv[])
+{
+
+	//Inicia todas as camadas 
+	int ret;
+
+	printf("Iniciando Camadas de Rede...\n");
+	ret = execl("enlace","hello","1","2","3",NULL);
+
+	printf("Failed execl...ret = %d\n",ret);
+
+
+	//Compilar o negocio estranho, fundi teste e enlace
+	return 0;
+}
+
+
+
---
+++ teste_proj.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+   struct cadastro
+{
+    char user[10];
+    char mens[50];    
+    int op;
+};
+
+/*
+ * Cliente UDP
+ */
+
+//Chama a funcao do enlace: enviaEnlace()
+
+int main(int argc, char *argv[])
+{
+
+	//Troca de mensagens ou fila, serve para simular a camada de rede.
+	enviaEnlace
+
+	return 0;
+
+}
+
+
