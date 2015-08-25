ef4b57828aee698794c49529e1e2371625b5dcbf
Louiz <luizhs396@gmail.com>
2015-08-25 09:32:59 -0300

    Join Files

--- Redes/enlace.c
+++ Redes/enlace.c
@@ -1,131 +1,148 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdio.h>    /* for printf()*/ 
+#include <stdlib.h>   /* for exit(1);*/
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <errno.h>              /* errno and error codes */
+#include <sys/time.h>           /* for gettimeofday() */
+#include <stdio.h>              /* for printf() */
+#include <unistd.h>             /* for fork() */
+#include <sys/types.h>          /* for wait() */
+#include <sys/wait.h>           /* for wait() */
+#include <signal.h>             /* for kill(), sigsuspend(), others */
+#include <sys/ipc.h>            /* for all IPC function calls */
+#include <sys/shm.h>            /* for shmget(), shmat(), shmctl() */
+#include <sys/sem.h>            /* for semget(), semop(), semctl() */
+#include <string.h>
+#include <sys/msg.h>    /* for msgget(), msgctl() */     
+#include <sys/types.h>    /* for wait(), msgget(), msgctl() */   
 
 
 
 /*
  * Cliente UDP
  */
-/*int main( int argc,char *argv[])
-{
-    int i;
-
-    for(i=0; i < argc; i++){
-      printf("argv[%d] = %s\n", i , argv[i]);
-
-    }
-
-    
-    
-    return 0;
-}*/
-//Carregar arq de config
-//Escutar na porta
 
 
+int matriz[10][10];
+int mtu[10][10];
 
 
+void print_matriz(int,int);
+
+void print_mtu(int,int);
+
+int tem_ligacao(int, int);
+
+int getMtu(int,int);
+
+
+struct NO{
+
+    int no;
+    int porta;
+    char ip[6];
+
+};
+
+struct NO nos[6];
 
 int iniciaEnlace(int no){
 
-	//Se o pacote maior que MTU retorna erro  ... MTU: quanto a camada de cima pode enviar.
+    printf("Erro, nao foi possivel abrir o arquivo\n");
+    
+char url[]="arq.config",
+       nome[20];
+    
+    printf("Erro, nao foi possivel abrir o arquivo222\n");
 
-	//Nao deve fragmentar
+  int node=0;     
+  int number; 
+  int elem1;
+  int elem2;
+  int elem3;
+  int elem4; 
+  int porta;
 
-	//Se tiver menor processa
-
-
-	//Verifica se o no é vizinho, se nao for, retorn mensagem pra camada de cima
-
-	//Apenas verifica se o pacote esta certo, caso contrario joga fora, camada de transporte que deve se preucupar com isso
-
-
-	//dispara THREADS de envio e recebimento 
-
-	//Carrega arquivo de  Configuracao.
-
- printf("No do enlace: %d\n\n", no);
-	
-  /*int i,count;
-  
-  int ids_barbers[NUM_CLIENTS];
-  int ids_clients[NUM_CLIENTS];
-  int tb,tc;
-   
-  pthread_mutex_init(&exc_aces,NULL);
  
-  for(count=0;count < NUM_OF_CHINDREN;count++)
-  {
+ 
+  char *s;
+  FILE *arq;
+  
+  int ret=6;
+printf("TESTE\n");
     
+  arq = fopen(url, "r");
     
-    if(count < NUM_BARBERS){
+    printf("VA PORRA\n");
+  if(arq == NULL)
+      printf("Erro, nao foi possivel abrir o arquivo\n");
+  else{
+
+        printf("VA PORRA22\n");
       
-     ids_barbers[count]=count;
-    
-     tb=pthread_create(&barbeiros[count],NULL,barbeiro,(void*)&ids_barbers[count]); 
+       // fscanf(arq,"%s",s);
+
+        
+          do{
+             
+              
+            ret = fscanf(arq,"%d: IP = %d.%d.%d.%d, Porta = %d;", &number,&elem1,&elem2,&elem3,&elem4,&porta);
+            
+            if(ret >= 6){
+              printf("%d\n%d.%d.%d.%d\n%d\n", number,elem1,elem2,elem3,elem4,porta);
+
+            
+             nos[node].no = number;
+             nos[node].porta = porta;
+
+             sprintf(nos[node].ip,"%d.%d.%d.%d",elem1,elem2,elem3,elem4);
+            //printf("\nFRI:%s\n\n", fri);
+             node++;
+          }
+          }while(ret >= 6);
+
+        ret=3;
+        fscanf(arq,"%s",s);
+
+        do{
+            ret = fscanf(arq,"%d -> %d, MTU = %d;", &number,&elem1,&elem2);
+            
+            if(ret >= 3){
+              printf("%d -> %d, MTU = %d;\n", number,elem1,elem2);
+              matriz[number-1][elem1-1]=1;
+              matriz[elem1-1][number-1]=1;
+              mtu[number-1][elem1-1]=elem2;
+              mtu[elem1-1][number-1]=elem2;
+            }
+          }while(ret >= 3);
+
+        
+        printf("\n");
+  
+
  
-        if(tb){
-          fprintf(stderr,"ERRO!Thread Barbeiro não criada");
-          exit(-1);
-        }
-    
-    }
-    
-    if(count < NUM_CLIENTS){
-      
-    ids_clients[count]=count;
-    
-    tc=pthread_create(&clientes[count],NULL,cliente,(void *)&ids_clients[count]); 
-    
-      	if(tc){
-      	fprintf(stderr,"ERRO!Thread Cliente não criada");
-      	 exit(-1);
-        }
-    }
-    else{
-    //  printf("contC=%d\n",contC);
-      break;//pthread_join(clientes[count],NULL);
-    }
-    //Inicia a thread que escuta a porta e quando recebe ela abre outra que faz a conta de verificacao e envia para teste.c
+}
 
 
-  }
-  //////////////////////////////////////////////////////////////////////////////Em cima esta a criação.
-   
- for(i=0;i < NUM_CLIENTS;i++){
-      
-    //pthread_join(clientes[count],NULL);
-     pthread_join(clientes[i],NULL);
-  } 
- 
- for(i=0;i < NUM_BARBERS;i++){
-    tb=pthread_kill(barbeiros[i],0);
-    
-    
- 
-    if(tb){
-      fprintf(stderr,"ERRO!Thread Barbeiro não finalizada");
-      exit(-1);
-    }
+//Testando funcoes
+
+  printf("%d: IP:::::::::%s,PORTA = %d\n",nos[0].no,nos[0].ip,nos[0].porta );
+  printf("%d: IP:::::::::%s,PORTA = %d\n",nos[1].no,nos[1].ip,nos[1].porta );
+  printf("%d: IP:::::::::%s,PORTA = %d\n",nos[2].no,nos[2].ip,nos[2].porta );
+  printf("%d: IP:::::::::%s,PORTA = %d\n",nos[3].no,nos[3].ip,nos[3].porta );
+  fclose(arq);
+
+  printf("\n");
+  print_matriz(4,4);
+  printf("\n");
+  print_mtu(4,4);
+  printf("\n");
   
-  }
+  printf("\ntem ligacao:%d\n\n", tem_ligacao(1,2));
 
-  
-  pthread_mutex_destroy(&exc_aces);
-  
+  printf("\nMTU:%d\n\n", getMtu(1,2));
 
-  exit(1);
-  
-  
-//QUando a thread crianda aqui reecebe de thread de enviaenlace, cria-se outra thread que faz o calculo de verificacao e envia para a camada de teste.
-*/
-
-
-    return 0;
+  return 0;
 }
 
 
@@ -135,3 +152,51 @@ int enviaEnlace(int No, char datagrama[], int tamanho){
   //dispara uma thrread que envia para um nó. da thread crriada em inicia enlace
 	return 0;
 }
+
+void print_matriz(int lin, int col){
+
+
+    for(int x=0;x<lin;x++)
+    {
+        for(int y=0;y<col;y++)
+        {
+            printf("%d ", matriz[x][y]);
+        }
+
+        printf("\n");
+    }
+
+
+}
+
+void print_mtu(int lin, int col){
+
+
+  for(int x=0;x<lin;x++)
+    {
+        for(int y=0;y<col;y++)
+        {
+            printf("%d ", mtu[x][y]);
+        }
+
+        printf("\n");
+    }
+
+}
+
+int tem_ligacao(int no1,int no2){
+
+    if(matriz[no1][no2])
+      return 1;
+    return 0;
+
+}
+
+int getMtu(int no1, int no2){
+
+    if(!tem_ligacao(no1,no2))
+      return 0;
+    return mtu[no1][no2];
+
+}
+
--- Redes/teste.c
+++ Redes/teste.c
@@ -1,14 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdio.h>    /* for printf()*/ 
+#include <stdlib.h>   /* for exit(1);*/
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <errno.h>              /* errno and error codes */
+#include <sys/time.h>           /* for gettimeofday() */
+#include <stdio.h>              /* for printf() */
+#include <unistd.h>             /* for fork() */
+#include <sys/types.h>          /* for wait() */
+#include <sys/wait.h>           /* for wait() */
+#include <signal.h>             /* for kill(), sigsuspend(), others */
+#include <sys/ipc.h>            /* for all IPC function calls */
+#include <sys/shm.h>            /* for shmget(), shmat(), shmctl() */
+#include <sys/sem.h>            /* for semget(), semop(), semctl() */
+#include <string.h>
+#include <sys/msg.h>    /* for msgget(), msgctl() */     
+#include <sys/types.h>    /* for wait(), msgget(), msgctl() */   
 
 
 /*
  * Cliente UDP
  */
+
+
+
+
 
 //Chama a funcao do enlace: enviaEnlace()
 
